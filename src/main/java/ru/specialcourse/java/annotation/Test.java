4cbf165e2bc13caff1083aa70acf6460e0709a38
Declow <declow@mail.ru>
2017-09-10 16:21:36 +0700

    Добавлена аннотация Test

--- src/main/java/ru/specialcourse/java/annotation/Test.java
+++ src/main/java/ru/specialcourse/java/annotation/Test.java
@@ -1,4 +1,17 @@
 package ru.specialcourse.java.annotation;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Target(value = ElementType.METHOD)
+@Retention(value = RetentionPolicy.RUNTIME)
 public @interface Test {
+    class None extends Throwable {
+        private None() {
+        }
+    }
+
+    Class<? extends Throwable> expected() default None.class;
 }
