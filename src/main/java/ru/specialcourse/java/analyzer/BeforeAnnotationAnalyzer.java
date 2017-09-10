c3971b20749f5ec840d1c6a2f08f6cce9fae371b
Dmitry Gerasimenko <astronautr@gmail.com>
2017-09-10 14:52:23 +0700

    Fix review issues, small code refactor

--- src/main/java/ru/specialcourse/java/analyzer/BeforeAnnotationAnalyzer.java
+++ src/main/java/ru/specialcourse/java/analyzer/BeforeAnnotationAnalyzer.java
@@ -8,9 +8,9 @@ import java.lang.reflect.Method;
 public class BeforeAnnotationAnalyzer implements AnnotationAnalyzer {
     private static final byte MAX_BEFORE_ANNOTATIONS = 1;
 
-    public void analyze(Class<?> cl) throws MultipleBeforeAnnotationException {
-        int beforeAnnotations = 0;
-        Method[] methods = cl.getMethods();
+    public void analyze(Class<?> clazz) throws MultipleBeforeAnnotationException {
+        byte beforeAnnotations = 0;
+        Method[] methods = clazz.getMethods();
 
         for (Method method: methods) {
             if (method.isAnnotationPresent(Before.class)) {
--- src/main/java/ru/specialcourse/java/exception/MultipleBeforeAnnotationException.java
+++ src/main/java/ru/specialcourse/java/exception/MultipleBeforeAnnotationException.java
@@ -1,7 +1,7 @@
 package ru.specialcourse.java.exception;
 
 public class MultipleBeforeAnnotationException extends MultipleAnnotationException {
-    private static final String MESSAGE = "Multiple @Before annotation doesn't allowed here.";
+    private static final String MESSAGE = "Multiple @Before annotation doesn't allowed here";
 
     public MultipleBeforeAnnotationException() {
         super(MESSAGE);
